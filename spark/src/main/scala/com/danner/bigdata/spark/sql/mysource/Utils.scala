d173484168e70ee94aa02a7fe680c27a61b280b4
arrow <18042304384@163.com>
2021-03-29 07:52:38 +0800

    Update Utils.scala

--- spark/src/main/scala/com/danner/bigdata/spark/sql/mysource/Utils.scala
+++ spark/src/main/scala/com/danner/bigdata/spark/sql/mysource/Utils.scala
@@ -2,10 +2,6 @@ package com.danner.bigdata.spark.sql.mysource
 
 import org.apache.spark.sql.types._
 
-/**
-  * 讲师：若泽(PK哥)
-  * 交流群：126181630
-  */
 object Utils {
 
   def castTo(value:String, dataType:DataType) ={

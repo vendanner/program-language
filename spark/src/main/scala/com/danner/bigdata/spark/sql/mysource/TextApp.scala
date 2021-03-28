0bea620af05a6b8cbccf5f4e8681f687f619fdb2
arrow <18042304384@163.com>
2021-03-29 07:52:35 +0800

    Update TextApp.scala

--- spark/src/main/scala/com/danner/bigdata/spark/sql/mysource/TextApp.scala
+++ spark/src/main/scala/com/danner/bigdata/spark/sql/mysource/TextApp.scala
@@ -1,12 +1,19 @@
 package com.danner.bigdata.spark.sql.mysource
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.types._
 
 /**
   */
 object TextApp {
 
   def main(args: Array[String]): Unit = {
+
+    val schema = StructType(
+      StructField("id",IntegerType,false) ::
+        StructField("name",StringType,false) :: Nil
+    )
 
     val spark = SparkSession.builder().appName("TextApp").master("local[2]").getOrCreate()
 
@@ -16,7 +23,8 @@ object TextApp {
     spark.sql("select * from test where comm < 300").show()
     df.persist()
 
-
+   val row =  new GenericRowWithSchema(Array(Seq(1),Seq("li")), schema)
+    print(row.getAs[Int]("id"))
     Thread.sleep(100000)
     spark.stop()
   }
